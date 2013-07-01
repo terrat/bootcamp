08f2e5a18dcec31c881d02d7769fb8f10ea80dcd
Terra.Thompson <Terra.Thompson@noaa.gov>
2013-07-01 14:37:04 -0500

    adding more numbers

--- add_numb.py
+++ add_numb.py
@@ -1,10 +1,12 @@
 
 # This has been authored by Terra but it came from Aleksandra in boot camp.
+
 def add_two(a,b):
    return a+b
 
 def main():
    print ("2 + 3 = ",add_two(2,3))
+   print ("4 + 4 = ",add_two(4,4))
 
 if __name__ == "__main__":
      main()
